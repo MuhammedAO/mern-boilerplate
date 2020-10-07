98cc9ec8b4d8135f286a55038dbf51d3d6b1e358
Muhammed ogunsanya <mspeaks910@gmail.com>
2020-10-07 23:01:51 +0100

    connect to mongoDB

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 const express = require('express')
-
 const app = express()
+
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb+srv://mo:xxxx@cluster0.q9rcm.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => console.log('Db Connected'))
+  .catch((err) => console.log(err))
+
+// const mongooseUrl = 'mongodb+srv://mo:xxxx@cluster0.q9rcm.mongodb.net/<dbname>?retryWrites=true&w=majority'
+
+// const dbConnect = async () => {
+//   try {
+//     const endPoint = await mongoose.connect(mongooseUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+//     return endPoint ? console.log('Db connect') : ''
+//   }
+//   catch (error) {
+//     console.log(error)
+//   }
+// }
+
+// dbConnect()
+
 
 app.get('', (req, res) => {
   res.send('Hello World')
