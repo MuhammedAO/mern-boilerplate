78c94ac5f1126795540a2cfc263c2c27dbb83306
Muhammed ogunsanya <mspeaks910@gmail.com>
2020-11-10 13:44:55 +0100

    heroku

--- server/index.js
+++ server/index.js
@@ -80,6 +80,6 @@ app.get('/api/user/logout', auth, (req, res) => {
   })
 })
 
-const PORT = 5000
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
